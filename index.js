a774798151feb34635a912bf15ba6b19c9f91b4b
Harsh Verdhan Singh <harshverdhansingh1@gmail.com>
2024-07-09 05:49:25 +0530

    user model done

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
 
 
-app.listen(3000,()=>{
+
+port = process.env.PORT || 3000;
+app.listen(port,()=>{
     console.log("Server started on port 3000");
 })
